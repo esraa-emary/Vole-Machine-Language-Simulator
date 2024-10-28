b6e826b46716b2a4fbf957afb74a9234bed48824
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-10-28 18:23:44 +0300

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -3,15 +3,11 @@
 
 #include <bits/stdc++.h>
 #include "Instructions.h"
+
 using namespace std;
 
 class Machine {
-private:
-    Instructions input;
-    int program_count;
 public:
-    Machine();
-    string Get_Next_Instruction();
     void Run_Instruction();
 };
 
