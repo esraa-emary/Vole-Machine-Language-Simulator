0fb7967043dba0b707aa8d6480d898c4af8f5d76
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-10-28 17:49:13 +0300

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -2,51 +2,11 @@
 #define MACHINE_H
 
 #include <bits/stdc++.h>
-
+#include "Instructions.h"
 using namespace std;
-
-// =========================== Memory ===========================
-
-class Memory {
-private:
-    std::array<int, 256> memory;
-public:
-    Memory();
-    int getMemory(int address);
-    void setMemory(int address, int value);
-    void printMemory();
-    void Display_Memory();
-};
-
-// =========================== Register ===========================
-
-class Register {
-private:
-    vector<string> registers = vector<string>(16);
-public:
-    Register();
-    void setRegister(const string &Address, const string &Value);
-    string getRegister(const string &location);
-    void displayRegister();
-    void clearRegister();
-};
-
-// =========================== Instructions ===========================
-
-class Instructions {
-private:
-    vector<string> instruct;
-public:
-    vector<string> Read_From_File();
-    void Decode();
-};
-
-// =========================== Machine ===========================
 
 class Machine {
 private:
-    Register processor;
-    Memory storage;
     Instructions input;
     int program_count;
 public:
