8e8deaa50e39ab7e5a85fe462b771be00a2f4323
Nagham Wael <161651186+NaghamProgrammer@users.noreply.github.com>
2024-10-28 12:44:23 +0300

    Create Memory.cpp

---
+++ Memory.cpp
@@ -0,0 +1,31 @@
+#include "Memory.h"
+using namespace std;
+// Constructor to initialize the array with 0 in all cells
+Memory::Memory() {
+    for (int i = 0; i < 256; i++) {
+        memory[i] = 0;
+    }
+}
+
+// Getter to read from memory
+int Memory::getMemory(int address) {
+    if (address < 0 || address >= 256) {
+        throw out_of_range("Memory read error: Address out of range");
+    }
+    return memory[address];
+}
+
+// Setter to write in the memory
+void Memory::setMemory(int address, int value) {
+    if (address < 0 || address >= 256) {
+        throw out_of_range("Memory write error: Address out of range");
+    }
+    memory[address] = value;
+}
+
+// Display memory contents
+void Memory::printMemory() {
+    for (int i = 0; i < 256; ++i) {
+        cout << "Memory[" << i << "] = " << memory[i] << "\n";
+    }
+}
