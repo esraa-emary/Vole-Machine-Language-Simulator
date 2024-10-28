f6147c87b2a1b4162c10a1d3691c30e37e187e4a
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-10-28 21:50:26 +0300

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -3,12 +3,18 @@
 
 #include <bits/stdc++.h>
 #include "Instructions.h"
+#include "Register.h"
+#include "Memory.h"
 
 using namespace std;
 
 class Machine {
 public:
     void Run_Instruction();
+    void Move(string address1, string address2, Register &reg);
+    void Load_To_Register(string address1, string value, Register &reg);
+    void Load_From_Memory_To_Register(string address4, string address1, Register &reg, Memory &mem);
+    void Store(string address1, string address2, Register & reg, Memory & mem);
 };
 
 #endif // MACHINE_H
