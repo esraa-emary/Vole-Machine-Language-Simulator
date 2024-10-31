9c9fec69a3c73781b0cb96414e89fed1ac8de3f3
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-10-31 14:29:03 +0300

    Update Machine.cpp

--- Machine.cpp
+++ Machine.cpp
@@ -8,27 +8,6 @@
 using namespace std;
 
 
-void Machine::Load_From_Memory_To_Register(string address4, string address1, Register &reg, Memory &mem) {
-    int index = Hexa_To_Decimal(address4);
-    string content = mem.getMemory(index);
-    reg.setRegister(address1,content);
-}
-
-void Machine::Load_To_Register(string address1, string value, Register &reg) {
-    reg.setRegister(address1, value);
-}
-
-void Machine::Store(string address1, string address4, Register &reg, Memory &mem) {
-    string content = reg.getRegister(address1);
-    int address = Hexa_To_Decimal(address4);
-    mem.setMemory(address, content);
-}
-
-void Machine::Move(string address2, string address3, Register &reg) {
-    string content = reg.getRegister(address2);
-    reg.setRegister(address3, content);
-}
-
 void Machine::Run_Instruction() {
     Memory mem;
     Register reg;
@@ -41,22 +20,22 @@ void Machine::Run_Instruction() {
         string address3 = string(1, instructions[i][3]);
         string address4 = instructions[i].substr(2, 2);
         if (instructions[i][0] == '1') {
-            Load_From_Memory_To_Register(address4, address1,reg,mem);
+            inst.Load_From_Memory_To_Register(address4, address1,reg,mem);
         } else if (instructions[i][0] == '2') {
             string value = instructions[i].substr(2, 2);
-            Load_To_Register(address1, value, reg);
+            inst.Load_To_Register(address1, value, reg);
         } else if (instructions[i][0] == '3') {
-            Store(address1, address4,reg,mem);
+            inst.Store(address1, address4,reg,mem);
         } else if (instructions[i][0] == '4') {
-            Move(address2, address3, reg);
+            inst.Move(address2, address3, reg);
         } else if (instructions[i][0] == '5') {
-            AddingTwoComplement(address1, address2, address3, reg);
+            inst.AddingTwoComplement(address1, address2, address3, reg);
         } else if (instructions[i][0] == '6') {
 
         } else if (instructions[i][0] == '7') {
-            OrBitwiseOperation(address1, address2, address3, reg);
+            inst.OrBitwiseOperation(address1, address2, address3, reg);
         } else if (instructions[i][0] == '8') {
-            AndBitwiseOperation(address1, address2, address3, reg);
+            inst.AndBitwiseOperation(address1, address2, address3, reg);
         } else if (instructions[i][0] == '9') {
 
         } else if (instructions[i][0] == 'A') {
