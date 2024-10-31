b67bd7654ea9393dcfd0ee4c5d0f57f0e171f066
Mohammed Atef Abd El-Kader <161865675+Mohammed-3tef@users.noreply.github.com>
2024-10-31 14:16:45 +0300

    Update Instructions.cpp

--- Instructions.cpp
+++ Instructions.cpp
@@ -47,6 +47,123 @@ void Instructions::Read_From_File() {
     instruct = instructions;
 }
 
+int Instructions::Hexa_To_Decimal(string Hex_Number) {
+    // Convert the hexadecimal number to decimal.
+    int decimalValue = stoi(Hex_Number, nullptr, 16);
+    return decimalValue;
+}
+
+string Instructions::Decimal_To_Hexa (int Dec_Number) {
+    stringstream hexadecimal;
+    // Convert the decimal number to hexadecimal.
+    hexadecimal << hex << (static_cast<uint16_t>(Dec_Number) & 0xFF);
+    return hexadecimal.str();
+}
+
+string Instructions::OneComplement(string binary) {
+    while (binary.size() < 16) {binary = '0' + binary;}
+
+    // For ones complement flip every bit in the binary number.
+    for (int i = 0; i < binary.size(); ++i) {
+        binary[i] = (binary[i] == '0') ? '1' : '0';
+    }
+    return binary;
+}
+
+string Instructions::TwoComplement(string& binary) {
+    string onesComplement = OneComplement(binary);
+
+    // For twos complement add 1 to the ones complement.
+    int carry = 1;
+    for (int i = binary.size() - 1; i >= 0; --i) {
+        if (onesComplement[i] == '1' && carry == 1) onesComplement[i] = '0';
+        else if (onesComplement[i] == '0' && carry == 1) {
+            onesComplement[i] = '1';
+            carry = 0;
+        }
+    }
+    return onesComplement;
+}
+
+string Instructions::decimalToBinary(int num) {
+    string bin = "";
+    bool isNegative = false;
+    
+    // If the number is zero, return 0.
+    if (num == 0) {return "0";}
+    // If the number is negative, convert it to positive and set the isNegative flag to true.
+    else if (num < 0) {
+        isNegative = true, num *= -1;
+    }
+    
+    // Convert the decimal number to binary.
+    while (num > 0) {
+        bin = char('0' + num % 2) + bin;
+        num /= 2;
+    }
+    
+    // Make the binary number have 16 bits.
+    while (bin.size() < 16) {bin = '0' + bin;}
+    return isNegative ? TwoComplement(bin) : bin;
+}
+
+string Instructions::AddingBinaryNumbers(string binary1, string binary2) {
+    string result;
+    int carry = 0;
+    binary1 = '0' + binary1, binary2 = '0' + binary2;
+
+    // Make the two binary numbers have the same size.
+    while (binary1.size() > binary2.size()) {binary2 = '0' + binary2;}
+    while (binary2.size() > binary1.size()) {binary1 = '0' + binary1;}
+
+    // Adding the two binary numbers.
+    for (int i = binary1.size() - 1; i >= 0; --i) {
+        int sum = (binary1[i] - '0') + (binary2[i] - '0') + carry;
+        if (sum == 0) {
+            result.push_back('0');
+            carry = 0;
+        }
+        else if (sum == 1) {
+            result.push_back('1');
+            carry = 0;
+        }
+        else if (sum == 2) {
+            result.push_back('0');
+            carry = 1;
+        }
+        else if (sum == 3) {
+            result.push_back('1');
+            carry = 1;
+        }
+    }
+
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+void Instructions::AddingTwoComplement(string& address1, string& address2, string& address3, Register& reg) {
+    int valReg1 = Hexa_To_Decimal(reg.getRegister(address2));
+    int valReg2 = Hexa_To_Decimal(reg.getRegister(address3));
+    string binary1 = decimalToBinary(valReg1);
+    string binary2 = decimalToBinary(valReg2);
+    string result = AddingBinaryNumbers(binary1, binary2);
+    reg.setRegister(address1, Decimal_To_Hexa(Hexa_To_Decimal(result)));
+}
+
+void Instructions::OrBitwiseOperation(string& address1, string& address2, string& address3, Register& reg) {
+    int valReg1 = Hexa_To_Decimal(reg.getRegister(address2));
+    int valReg2 = Hexa_To_Decimal(reg.getRegister(address3));
+    int result = valReg1 | valReg2;
+    reg.setRegister(address1, Decimal_To_Hexa(result));
+}
+
+void Instructions::AndBitwiseOperation(string& address1, string& address2, string& address3, Register& reg) {
+    int valReg1 = Hexa_To_Decimal(reg.getRegister(address2));
+    int valReg2 = Hexa_To_Decimal(reg.getRegister(address3));
+    int result = valReg1 & valReg2;
+    reg.setRegister(address1, Decimal_To_Hexa(result));
+}
+
 void Instructions::exclusiveOr(int R, int S, int T) {
     int result = getRegister(S) ^ getRegister(T);
     setRegister(R, result);
