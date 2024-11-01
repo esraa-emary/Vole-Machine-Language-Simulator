bab2e626ea0057915bf80bee1eab813e041ed217
Mohammed Atef Abd El-Kader <161865675+Mohammed-3tef@users.noreply.github.com>
2024-11-01 18:24:59 +0200

    Update Instructions.h
    Added Implicit Normalization Addition ...

--- Instructions.h
+++ Instructions.h
@@ -16,20 +16,29 @@ public:
     void Read_From_File();
     vector<string> Get_Instructions();
     Instructions(){}
+
+    string OneComplement(string binary);
+    string twoComplement(string binary);
+    string decimalToBinary(int Dec_Number);
+    int Hexa_To_Decimal(string Hex_Number);
+    string Decimal_To_Hexa (int Dec_Number);
+    int binaryToDecimal(string binNumber);
+    double binaryToDecimalFraction(const string& binaryFraction);
+    string fractionDecimalToBinary(double decimalFraction, int precision);
+
     void Move(string address1, string address2, Register &reg);
     void Load_To_Register(string address1, string value, Register &reg);
     void Load_From_Memory_To_Register(string address4, string address1, Register &reg, Memory &mem);
     void Store(string address1, string address2, Register & reg, Memory & mem);
-    int Hexa_To_Decimal(string Hex_Number);
-    string Decimal_To_Hexa (int Dec_Number);
-    string OneComplement(string binary);
-    string TwoComplement(string& binary);
-    string decimalToBinary(int num);
-    int binaryToDecimal(string binNumber);
+
     string AddingBinaryNumbers(string binary1, string binary2);
     void AddingTwoComplement(string& address1, string& address2, string& address3, Register& reg);
+    string ImplicitNormalization(double Dec_Number);
+    double encodeImplicitNormalization(string hexNumber);
+    string AddingFloatingNumber(string& address1, string& address2, string& address3, Register& reg);
     void OrBitwiseOperation(string& address1, string& address2, string& address3, Register& reg);
     void AndBitwiseOperation(string& address1, string& address2, string& address3, Register& reg);
+
     void halt(bool& haltFlag);
     void rotateRight(const string& R, int X, Register& reg);
     void conditionalJump(const string& R, int XY, Register& reg, int& currentInstructionIndex);
