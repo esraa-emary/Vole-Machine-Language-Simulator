ef1f359a95375c644b096cfc1f7456e94c6e44ab
Nagham Wael <161651186+NaghamProgrammer@users.noreply.github.com>
2024-11-02 10:17:44 +0200

    Update main.cpp
    added menu options

--- main.cpp
+++ main.cpp
@@ -4,11 +4,41 @@
 using namespace std;
 
 int main() {
-    cout << "Welcome To Our Vole Machine Simulator Program!\n";
+    cout << "Welcome to Our Vole Machine Simulator Program!\n";
     Machine m;
-    m.Run_Instruction();
-    m.getRegister();
-    m.getMemory();
-    cout << "Thanks for using our program!";
+    int choice;
+    bool running = true;
+
+    while (running) {
+        cout << "\nMenu:\n";
+        cout << "1) Load new program\n";
+        cout << "2) Run program as a whole\n";
+        cout << "3) Run step by step\n";
+        cout << "4) Exit\n";
+        cout << "Choose an option: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "\nLoading a new program...\n";
+                m.LoadNewProgram();
+                break;
+            case 2:
+                cout << "\nRunning program as a whole...\n";
+                m.RunAll();
+                break;
+            case 3:
+                cout << "\nRunning program step by step...\n";
+                m.RunStepByStep();
+                break;
+            case 4:
+                cout << "\nExiting the program. Thank you for using our simulator!\n";
+                running = false;
+                break;
+            default:
+                cout << "\nInvalid choice. Please select a valid option.\n";
+                break;
+        }
+    }
     return 0;
 }
