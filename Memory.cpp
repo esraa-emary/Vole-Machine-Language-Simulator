80821e870170a20c1bf9405d77fa193516a966fa
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-11-05 21:55:41 +0200

    Update Memory.cpp

--- Memory.cpp
+++ Memory.cpp
@@ -1,7 +1,9 @@
 #include "Memory.h"
 
 #include <bits/stdc++.h>
+
 using namespace std;
+
 // Constructor to initialize the array with 0 in all cells
 Memory::Memory() {
     for (int i = 0; i < 256; i++) {
@@ -42,4 +44,78 @@ void Memory::printMemory() {
         }
         cout << endl; // Move to the next line after each row
     }
+}
+
+bool Memory::checkDecimal(const string &str) {
+    for (char i: str) {
+        // Check if the character is a valid decimal number.
+        if (!isdigit(i)) return false;
+    }
+    return true;
+}
+
+bool Memory::checkProgramCounter(int programCounter, vector<string> &instructions) {
+    return programCounter + (instructions.size() * 2) < 256;          // Check if the program fits in the memory.
+}
+
+void Memory::programCounterIncrement(vector<std::string> &instructions) {
+    int numberOfAttempts = 3;
+    int proCou;
+    while (true) {
+        string address;
+
+        if (numberOfAttempts > 0) {
+            cout << "\nTake care, the Number of attempts = " << numberOfAttempts << endl;
+            cout << "Enter Program Counter :";
+            getline(cin, address);
+        }
+
+            // If the user exceeds the number of attempts, set the program counter to 10.
+        else if (numberOfAttempts == 0) {
+            cout << "\nYou have exceeded the number of attempts.\n";
+            cout << "So, The Default Program Counter = 10.\n";
+            address = "10";
+        }
+
+        if (checkDecimal(address)) {
+            proCou = stoi(address);        // Convert the decimal address to decimal.
+
+            // Check if the address is in the range of the memory.
+            if (checkProgramCounter(proCou, instructions) && proCou % 2 == 0 && proCou > 0) {
+                programCounter = proCou;
+                // Load the program into the memory.
+                for (int i = 0; i < instructions.size(); ++i) {
+                    memory[proCou] = instructions[i].substr(0, 2);
+                    ++proCou;
+                    memory[proCou] = instructions[i].substr(2);
+                    ++proCou;
+                }
+                break;
+            }
+
+                // If the address is not in the range of the memory, ask the user to enter a valid address.
+            else {
+                cout << "\nThe address is out of range. Please enter a valid address.\n";
+                --numberOfAttempts;
+            }
+        }
+
+            // If the address is not a decimal number, ask the user to enter a valid address.
+        else {
+            cout << "\nThe address is out of range. Please enter a valid address.\n";
+            --numberOfAttempts;
+        }
+    }
+}
+
+void Memory::setProgramCounter(int address) {
+    programCounter = address;
+}
+
+int Memory::getProgramCounter() {
+    return programCounter;
+}
+
+string Memory::getInstruction() {
+    return memory[programCounter] + memory[programCounter + 1];
 }
