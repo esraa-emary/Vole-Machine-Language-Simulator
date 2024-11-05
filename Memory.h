1723ea6109cd4ddd784bf7947e360ac1c1fb88f8
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-11-05 21:56:53 +0200

    Update Memory.h

--- Memory.h
+++ Memory.h
@@ -7,12 +7,18 @@ using namespace std;
 class Memory {
 private:
     std::array<string , 256> memory;
-
+    int programCounter = 10;
 public:
     Memory();
     string getMemory(int address);
     void setMemory(int address, string value);
-    void printMemory();                
+    void printMemory();
+    void programCounterIncrement (vector<string>& instructions);
+    bool checkDecimal(const string& str);
+    bool checkProgramCounter(int programCounter, vector<string>& instructions);
+    void setProgramCounter(int address);
+    int getProgramCounter ();
+    string getInstruction ();
 };
 
 #endif  
